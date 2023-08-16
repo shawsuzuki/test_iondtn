44e435aefdb3ba423d05eb90d9d64666461c95a5
shotasu <shota.ss1023@keio.jp>
2023-08-16 21:25:39 +0900

    clear

--- node_2/shaw_wip/ion_container_create.sh
+++
@@ -1,7 +0,0 @@
-docker rm -f $(docker ps -q -a)
-docker rmi -f $(docker images -q)
-
-docker-compose  build --no-cache
-
-docker run -it -d shaw_wip-dtn-server-1
-docker run -it -d shaw_wip-dtn-server-2
--- node_3/shaw_wip/ion_container_create.sh
+++
@@ -1,4 +0,0 @@
-docker-compose  build --no-cache
-
-docker run -it -d shaw_wip-dtn-server-1
-docker run -it -d shaw_wip-dtn-server-2
